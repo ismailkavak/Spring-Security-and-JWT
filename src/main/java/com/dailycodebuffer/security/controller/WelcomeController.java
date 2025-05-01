ffcd6eebd90f97ede281cbd6c940a1f7bb36e105
ismailkavak <poyrazkvk@hotmail.com>
2025-05-01 12:43:12 +0300

    Added get csrf token to WelcomeController

--- src/main/java/com/dailycodebuffer/security/controller/WelcomeController.java
+++ src/main/java/com/dailycodebuffer/security/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.dailycodebuffer.security.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +10,10 @@ public class WelcomeController {
     @GetMapping("")
     public String welcome(){
         return "Welcome Spring Security!";
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 }
