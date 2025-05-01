7d2117669dd58e6965054b734b6de30dde97c054
ismailkavak <poyrazkvk@hotmail.com>
2025-05-01 16:40:12 +0300

    Added SecurityFilterChain and UserDetailsService

---
+++ src/main/java/com/dailycodebuffer/security/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.dailycodebuffer.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                request -> request.anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails cr7 = User.withUsername("cr7")
+                .password("{noop}portugal")
+                .roles("USER")
+                .build();
+
+        UserDetails iso = User.withUsername("iso")
+                .password("{noop}portugal")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(cr7,iso);
+    }
+}
